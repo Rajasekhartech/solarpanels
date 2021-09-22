2621065596e60a312efe20eb43ce782167b1af75
Rajasekhar <rajasekhartechpy@gmail.com>
2021-09-22 07:50:20 +0530

    first

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1233783.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 55736408, 55736409, 55736410, 55736411, 55736412, 55736413, 55750539, 55752393, 55756714, 55756718, 55756720, 55756721, 55756797, 55756936, 55765500, 55765662, 55779946 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/55767156/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/55767157/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/55767155/1/ammo.js', 'preload' : true},
+];
